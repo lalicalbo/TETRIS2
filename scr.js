ac91f0dfeba92234123496f69046aa791d079c43
laura <ll.calderon14@gmail.com>
2021-07-06 15:40:23 -0500

    Se realiza la prueba por fin

---
+++ scr.js
@@ -0,0 +1,14 @@
+console.log("cargando");
+
+
+
+function cambia(lau){
+    console.log ( "probando");
+    let color= document.getElementById("color1");
+    let cuadro=document.getElementById(lau);
+    cuadro.style.backgroundColor=color.value;
+
+}
+
+
+
